7bc8eb9b70bb1902368f5d963ac71ad62a458d59
davide <davide.galletti@gmail.com>
2022-07-15 20:12:17 +0200

    Initial commit

---
+++ oks2/keep_safe_sample.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Author: Davide Galletti                davide   ( at )   c4k.it
+
+# python3 -c 'import secrets; print(secrets.token_hex(100))'
+SECRET_KEY = '*************************************************************************'
+DATABASE_PASSWORD = '**********'
+DATABASE_NAME = '**********'
+
+# python3 -c 'import secrets; print(secrets.token_hex(100))'
+STAGE_SECRET_KEY = '*************************************************************************'
+STAGE_DATABASE_PASSWORD = '**********'
+STAGE_DATABASE_NAME = '**********'
+
+# SMTP
+EMAIL_HOST = 'smtp-.example.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = '**********@example.com'
+EMAIL_HOST_PASSWORD = '**********'
+
