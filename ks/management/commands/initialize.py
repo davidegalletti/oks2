ecef5b3ca57a82f44a0e1a5195ff3fd01d68d021
Davide Galletti <davide,galletti@gmail,com>
2025-02-12 19:48:06 +0100

    fixes #791 minimal license

--- ks/management/commands/initialize.py
+++ ks/management/commands/initialize.py
@@ -1,12 +1,44 @@
 # -*- coding: utf-8 -*-
 
-import logging, os, urllib.request
-from django.db import transaction
+import logging
 from django.core.management.base import BaseCommand
-from ks.models import KnowledgeServer, Organization
+from ks.api import GenericApi
+from ks.models import KnowledgeServer, License, Organization
+
 
 logger = logging.getLogger(__name__)
 
+
+def licenses(db_alias):
+    ga = GenericApi(url='https://licenses.opendefinition.org/licenses/groups/od.json')
+    ga.invoke()
+    for license in ga.decoded_response:
+        if License.objects.filter(short_name=license).exists():
+            l = License.objects.filter(short_name=license).get()
+        else:
+            l = License.objects.create(short_name=license)
+        license_data = ga.decoded_response[license]
+        if 'domain_content' in license_data:
+            l.domain_content = license_data['domain_content']
+        if 'domain_data' in license_data:
+            l.domain_data = license_data['domain_data']
+        if 'domain_software' in license_data:
+            l.domain_software = license_data['domain_software']
+        if 'id' in license_data:
+            l.legacy_id = license_data['id']
+        if 'maintainer' in license_data:
+            l.maintainer = license_data['maintainer']
+        if 'od_conformance' in license_data:
+            l.conformance_od = (license_data['od_conformance'] == 'approved')
+        if 'osd_conformance' in license_data:
+            l.conformance_osd = (license_data['osd_conformance'] == 'approved')
+        if 'status' in license_data:
+            l.active = (license_data['status'] == 'active')
+        if 'title' in license_data:
+            l.name = license_data['title']
+        if 'url' in license_data:
+            l.url = license_data['url']
+        l.save()
 
 def c4k_oks(db_alias):
     # this data must be created on the root ks but also on any other as it is essential for basic ks operation
@@ -47,5 +79,5 @@ class Command(BaseCommand):
         if db_alias in options:
             db_alias = options['db_alias']
         c4k_oks(db_alias)
-        # license()
+        licenses(db_alias)
         logger.info("END END END fixture END END END")
